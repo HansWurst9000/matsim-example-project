3c7026dc46152c733f9ef1fbe598ae46b06e7552
HansWurst9000 <skorpiondj@yahoo.de>
2022-10-13 14:48:24 +0200

    changed the package name

--- src/main/java/org/matsim/run/RunMatsimApplication.java
+++ src/main/java/org/matsim/run/RunMatsimApplication.java
@@ -16,7 +16,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package org.matsim.project;
+package org.matsim.run;
 
 import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 import org.matsim.api.core.v01.Scenario;
@@ -29,46 +29,46 @@ import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
  * @author nagel
  *
  */
-@CommandLine.Command( header = ":: MyScenario ::", version = "1.0")
-public class RunMatsimApplication extends MATSimApplication {
-
-	public RunMatsimApplication() {
-		super("scenarios/equil/config.xml");
-	}
-
-	public static void main(String[] args) {
-		MATSimApplication.run(RunMatsimApplication.class, args);
-	}
-
-	@Override
-	protected Config prepareConfig(Config config) {
-
-		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
-
-		// possibly modify config here
-
-		// ---
-
-		return config;
-	}
-
-	@Override
-	protected void prepareScenario(Scenario scenario) {
-
-		// possibly modify scenario here
-
-		// ---
-
-	}
-
-	@Override
-	protected void prepareControler(Controler controler) {
-
-		// possibly modify controler here
-
-//		controler.addOverridingModule( new OTFVisLiveModule() ) ;
-
-
-		// ---
-	}
-}
+//@CommandLine.Command( header = ":: MyScenario ::", version = "1.0")
+//public class RunMatsimApplication extends MATSimApplication {
+//
+//	public RunMatsimApplication() {
+//		super("scenarios/equil/config.xml");
+//	}
+//
+//	public static void main(String[] args) {
+//		MATSimApplication.run(RunMatsimApplication.class, args);
+//	}
+//
+//	@Override
+//	protected Config prepareConfig(Config config) {
+//
+//		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
+//
+//		// possibly modify config here
+//
+//		// ---
+//
+//		return config;
+//	}
+//
+//	@Override
+//	protected void prepareScenario(Scenario scenario) {
+//
+//		// possibly modify scenario here
+//
+//		// ---
+//
+//	}
+//
+//	@Override
+//	protected void prepareControler(Controler controler) {
+//
+//		// possibly modify controler here
+//
+////		controler.addOverridingModule( new OTFVisLiveModule() ) ;
+//
+//
+//		// ---
+//	}
+//}
